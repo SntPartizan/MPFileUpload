3f7c928933b2252b03ed3c838add392a46597e8f
Mikhail K <xpochta@gmail.com>
2023-11-15 20:23:11 +0600

    refactor: extract router code into separate class

---
+++ Router.py
@@ -0,0 +1,55 @@
+import datetime
+import hashlib
+import os
+
+import bottle
+from bottle import run, template, request
+
+
+def compute_hash(to_hash_str):
+    m = hashlib.md5()
+    m.update(to_hash_str.encode("utf-8"))
+    return m.hexdigest()
+
+
+class Router:
+
+    def __init__(self, uploader, upload_to):
+        self.uploader = uploader
+        self.upload_to = upload_to
+        bottle.route('/', 'GET', self.index)
+        bottle.route('/', 'POST', self.upload)
+
+        pass
+
+    def index(self):
+        ip = request.remote_addr
+        upload_id = "{}{}{}".format(datetime.datetime.now(), ip, request.headers.get('User-Agent'))
+        upload_id = compute_hash(upload_id)
+
+        return template('index.html', uid=upload_id)
+
+    def upload(self):
+        file = request.files.get("fileToUpload")
+        if file is None:
+            return "no file"
+
+        uid = request.params.get('uid')
+        params = {
+            "bucket": uid,
+            "last": request.params.get('last'),
+            "origin_file_name": request.params.get('origin'),
+            "file_name": file.filename,
+        }
+
+        upl_dir = os.path.join(self.upload_to, "uploads")
+
+        path = os.path.join(upl_dir, f"{uid}_{file.filename}")
+        file.save(path, overwrite=True)
+
+        self.uploader.save(path, **params)
+
+        return file.filename
+
+    def run(self, host, port):
+        run(host=host, port=port)
--- main.py
+++ main.py
@@ -1,60 +1,17 @@
-from bottle import route, run, template, post, request
 import os
-import hashlib
+
 from dotenv import load_dotenv
-import datetime
 
+from Router import Router
 from Uploader import Uploader
-
-load_dotenv()
-
-uploader = Uploader()
-
-proj_dir = os.path.dirname(os.path.abspath(__file__))
-
-
-def computeMD5hash(to_hash_str):
-    m = hashlib.md5()
-    m.update(to_hash_str.encode("utf-8"))
-    return m.hexdigest()
-
-
-@route('/')
-def index():
-    ip = request.remote_addr
-    upload_id = "{}{}{}".format(datetime.datetime.now(), ip, request.headers.get('User-Agent'))
-    upload_id = computeMD5hash(upload_id)
-
-    return template('index.html', uid=upload_id)
-
-
-@post("/")
-def post():
-    file = request.files.get("fileToUpload")
-    if file is None:
-        return "no file"
-
-    uid = request.params.get('uid')
-    params = {
-        "bucket": uid,
-        "last": request.params.get('last'),
-        "origin_file_name": request.params.get('origin'),
-        "file_name": file.filename,
-    }
-
-    upl_dir = os.path.join(proj_dir, "uploads")
-
-    path = os.path.join(upl_dir, f"{uid}_{file.filename}")
-    file.save(path, overwrite=True)
-
-    uploader.save(path, **params)
-
-    return file.filename
 
 
 def main():
-    run(host='localhost', port=8080)
-    # run(host='0.0.0.0', port=8080)
+    load_dotenv()
+    proj_dir = os.path.dirname(os.path.abspath(__file__))
+    uploader = Uploader()
+    router = Router(uploader=uploader, upload_to=proj_dir)
+    router.run(host='localhost', port=8080)
 
 
 if __name__ == "__main__":
