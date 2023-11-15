1d775d46648128fa07555c4e3115bcb6ffd79cb6
Mikhail K <xpochta@gmail.com>
2023-11-15 17:42:03 +0600

    fix: upload, combine, and cleanup of files

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import hashlib
 from minio import Minio
 from dotenv import load_dotenv
 import datetime
-from minio.commonconfig import ComposeSource
+from minio.commonconfig import ComposeSource, ENABLED, Filter
+from minio.deleteobjects import DeleteObject
+from minio.lifecycleconfig import Expiration, Rule, LifecycleConfig
 
 load_dotenv()
 
 secret = os.getenv('SECRET_KEY')
 
-bucket_name = "chunks"
+bucket_name = "uploaded"
 
 # Initialize minioClient with an endpoint and access/secret keys.
 minioClient = Minio('127.0.0.1:9000',
@@ -34,8 +36,6 @@ def index():
     upload_id = "{}{}{}".format(datetime.datetime.now(), ip, request.headers.get('User-Agent'))
     upload_id = computeMD5hash(upload_id)
 
-
-
     return template('index.html', uid=upload_id)
 
 
@@ -48,45 +48,77 @@ def post():
     uid = request.params.get('uid')
     last = request.params.get('last')
 
+    origin_file_name = request.params.get('origin')
+
     upl_dir = os.path.join(proj_dir, "uploads")
 
-    path = os.path.join(upl_dir, file.filename)
-    file.save(upl_dir, overwrite=True)
-
-    if last:
-        pass
+    path = os.path.join(upl_dir, f"{uid}_{file.filename}")
+    file.save(path, overwrite=True)
 
     print(uid)
-    save(path, file_name=file.filename, metadata={'uid': uid})
+    save(path, uid, file.filename)
+    if last == 'true':
+        objects = list(minioClient.list_objects(uid, "chunk"))
+        combine(uid, objects, origin_file_name)
+        remove_bucket(uid, objects)
 
     return file.filename
 
 
-def save(path, file_name="test", metadata=None):
+def remove_bucket(bucket, objects):
+    rem_list = [DeleteObject(x.object_name) for x in objects]
+    errors = minioClient.remove_objects(bucket, rem_list)
+    for error in errors:
+        print("error occurred when deleting object", error)
+
+    minioClient.remove_bucket(bucket)
+
+
+def make_bucket(name):
+    if minioClient.bucket_exists(name):
+        return
     try:
-        minioClient.fput_object(bucket_name, f"{metadata['uid']}_{file_name}", path, metadata=metadata)
+        minioClient.make_bucket(name)
     except Exception as err:
         print(err)
 
+    config = LifecycleConfig(
+        [
+            Rule(
+                ENABLED,
+                rule_filter=Filter(prefix="chunk"),
+                rule_id="ExpirationRule",
+                expiration=Expiration(days=1),
+            ),
+        ],
+    )
+    minioClient.set_bucket_lifecycle(name, config)
 
-if __name__ == "__main__":
 
+def save(path, bucket, file_name):
+    make_bucket(bucket)
+    try:
+        minioClient.fput_object(bucket, file_name, path)
+    except Exception as err:
+        print(err)
+    os.remove(path)
+
+
+def combine(bucket, objects, file_name):
+    sources = [ComposeSource(bucket, obj.object_name) for obj in objects]
+    result = minioClient.compose_object(bucket_name, file_name, sources)
+    print(f"{result.object_name =}")
+
+
+def main():
     if not minioClient.bucket_exists(bucket_name):
         try:
             minioClient.make_bucket(bucket_name)
         except Exception as err:
             print(err)
-
-    # l=minioClient.list_objects(bucket_name,"430fff6fa",recursive=True,include_user_meta=True)
-    #
-    # sources = [
-    #     ComposeSource(bucket_name, '430fff6fabf858740b589007a6e30e07_chunk_0'),
-    #     ComposeSource(bucket_name, '430fff6fabf858740b589007a6e30e07_chunk_1'),
-    #     ComposeSource(bucket_name, '430fff6fabf858740b589007a6e30e07_chunk_2'),
-    # ]
-    #
-    # # Create my-bucket/my-object by combining source object
-    # result = minioClient.compose_object(bucket_name, "my-object", sources)
-    # print(result.object_name, result.version_id)
-
     run(host='localhost', port=8080)
+    # run(host='0.0.0.0', port=8080)
+
+
+if __name__ == "__main__":
+    main()
