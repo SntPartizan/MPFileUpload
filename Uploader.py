53a25dfce8e2b777c15910d359a628f67df70ae6
Mikhail K <xpochta@gmail.com>
2023-11-15 18:47:58 +0600

    refactor: extract upload code into separate class

---
+++ Uploader.py
@@ -0,0 +1,62 @@
+import os
+from minio import Minio
+from minio.commonconfig import ENABLED, Filter, ComposeSource
+from minio.deleteobjects import DeleteObject
+from minio.lifecycleconfig import LifecycleConfig, Rule, Expiration
+
+
+class Uploader:
+
+    def __init__(self):
+        secret = os.getenv('SECRET_KEY')
+        self.bucket_name = 'uploaded'
+        self.minioClient = Minio('127.0.0.1:9000',
+                                 access_key='6VCT5WneTj6RmVEv5z31',
+                                 secret_key=secret,
+                                 region="test",
+                                 secure=False)
+
+        self.make_bucket(self.bucket_name)
+
+    def remove_bucket(self, bucket, objects):
+        rem_list = [DeleteObject(x.object_name) for x in objects]
+        errors = self.minioClient.remove_objects(bucket, rem_list)
+        for error in errors:
+            print("error occurred when deleting object", error)
+
+        self.minioClient.remove_bucket(bucket)
+
+    def set_lifecycle(self, bucket):
+        config = LifecycleConfig(
+            [
+                Rule(
+                    ENABLED,
+                    rule_filter=Filter(prefix="chunk"),
+                    rule_id="ExpirationRule",
+                    expiration=Expiration(days=1),
+                ),
+            ],
+        )
+        self.minioClient.set_bucket_lifecycle(bucket, config)
+
+    def make_bucket(self, name):
+        if self.minioClient.bucket_exists(name):
+            return
+        try:
+            self.minioClient.make_bucket(name)
+        except Exception as err:
+            print(err)
+
+    def save(self, path, bucket, file_name):
+        self.make_bucket(bucket)
+        self.set_lifecycle(bucket)
+        try:
+            self.minioClient.fput_object(bucket, file_name, path)
+        except Exception as err:
+            print(err)
+        os.remove(path)
+
+    def combine(self, bucket, objects, file_name):
+        sources = [ComposeSource(bucket, obj.object_name) for obj in objects]
+        result = self.minioClient.compose_object(self.bucket_name, file_name, sources)
+        print(f"{result.object_name =}")
--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 from bottle import route, run, template, post, request
 import os
 import hashlib
-from minio import Minio
 from dotenv import load_dotenv
 import datetime
-from minio.commonconfig import ComposeSource, ENABLED, Filter
-from minio.deleteobjects import DeleteObject
-from minio.lifecycleconfig import Expiration, Rule, LifecycleConfig
+
+from Uploader import Uploader
 
 load_dotenv()
 
-secret = os.getenv('SECRET_KEY')
+uploader = Uploader()
 
-bucket_name = "uploaded"
 
-# Initialize minioClient with an endpoint and access/secret keys.
-minioClient = Minio('127.0.0.1:9000',
-                    access_key='6VCT5WneTj6RmVEv5z31',
-                    secret_key=secret,
-                    region="test",
-                    secure=False)
+
+
 
 proj_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -56,66 +49,27 @@ def post():
     file.save(path, overwrite=True)
 
     print(uid)
-    save(path, uid, file.filename)
+    uploader.save(path, uid, file.filename)
     if last == 'true':
-        objects = list(minioClient.list_objects(uid, "chunk"))
-        combine(uid, objects, origin_file_name)
-        remove_bucket(uid, objects)
+        objects = list(uploader.minioClient.list_objects(uid, "chunk"))
+        uploader.combine(uid, objects, origin_file_name)
+        uploader.remove_bucket(uid, objects)
 
     return file.filename
 
 
-def remove_bucket(bucket, objects):
-    rem_list = [DeleteObject(x.object_name) for x in objects]
-    errors = minioClient.remove_objects(bucket, rem_list)
-    for error in errors:
-        print("error occurred when deleting object", error)
-
-    minioClient.remove_bucket(bucket)
 
 
-def make_bucket(name):
-    if minioClient.bucket_exists(name):
-        return
-    try:
-        minioClient.make_bucket(name)
-    except Exception as err:
-        print(err)
-
-    config = LifecycleConfig(
-        [
-            Rule(
-                ENABLED,
-                rule_filter=Filter(prefix="chunk"),
-                rule_id="ExpirationRule",
-                expiration=Expiration(days=1),
-            ),
-        ],
-    )
-    minioClient.set_bucket_lifecycle(name, config)
 
 
-def save(path, bucket, file_name):
-    make_bucket(bucket)
-    try:
-        minioClient.fput_object(bucket, file_name, path)
-    except Exception as err:
-        print(err)
-    os.remove(path)
 
 
-def combine(bucket, objects, file_name):
-    sources = [ComposeSource(bucket, obj.object_name) for obj in objects]
-    result = minioClient.compose_object(bucket_name, file_name, sources)
-    print(f"{result.object_name =}")
+
+
 
 
 def main():
-    if not minioClient.bucket_exists(bucket_name):
-        try:
-            minioClient.make_bucket(bucket_name)
-        except Exception as err:
-            print(err)
+
     run(host='localhost', port=8080)
     # run(host='0.0.0.0', port=8080)
 
